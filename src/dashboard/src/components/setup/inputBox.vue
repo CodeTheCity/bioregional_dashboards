9b61b2ef460044cd4ff34f7043116b1ca5b98b51
aboynejames <james@aboynejames.co.uk>
2021-03-21 09:11:23 +0000

    wikipedia query list and select

--- src/dashboard/src/components/setup/inputBox.vue
+++ src/dashboard/src/components/setup/inputBox.vue
@@ -12,14 +12,34 @@
       </form>
       <button @click.prevent="startBuild" class="button is-primary">Build bioregion</button>
     </div>
-    <div v-if="wikipage.data">
-      <div v-html="wikipage.data.query.pages[415350].extract">
+    <div v-if="querylist" id="search results">
+      <!-- Results {{ querylist }} -->
+      <div id="result-list" v-if="querylist.data !== undefined">
+        <ul  v-for="item of querylist.data[1]" :key='item.id'>
+          <li @click="selectEntry(item)">{{ item }}</li>
+        </ul>
+      </div>
+      <div id="selected-place">
+        Selected == {{ selectedPlace }}
+      </div>
+    </div>
+    <div id="wikipage-page" v-if="wikipage">
+      <div id="bioregion-message" v-if="selectedPlace.length > 2">
+        <div id="message-postive" v-if="matchMapData === true">
+          OK, display the map here and the toolbars
+        </div>
+        <div id="message-notready" v-if="matchMapData === false">
+          There is no bioregion prepared for this place.  Follow these steps to build out a map. . .
+          <button @click.prevent="buildMap" class="button is-primary">Build map</button>
+        </div>
+      </div>
+      <div v-html="wikipage">
       </div>
     </div>
     <div id="search results">
-      Results {{ querylist }}
+      river lists
+      {{ rawRiverpage }}
     </div>
-      wikipedia -- {{ wikipage }}
   </div>
 </template>
 
@@ -37,11 +57,15 @@ export default {
       forum: ''
     },
     wikipage: {},
-    querylist: []
+    querylist: [],
+    selectedPlace: '',
+    matchMapData: false,
+    rawRiverpage: ''
   }),
   methods: {
     async startBuild () {
       console.log('start build bioergion')
+      this.lookupRiversScotland()
       // move to lifeboard visualisation
       // this.$router.push('dashboard')
       let inputQuery = this.biointerest.text
@@ -53,20 +77,43 @@ export default {
       //  "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='New_England_Patriots'"
       let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search={ ' + inputQuery + '}&format=json'
       this.querylist = await axios.get(url)
-      console.log(this.queryWikiResults)
-      axios.get(url)
-        .then(
-          response => console.log(response.data)
-          // this.querylist = response.data
-        )
+    },
+    async selectEntry (place) {
+      console.log('place selected')
+      console.log(place)
+      this.selectedPlace = place
+      // query for specific wikipedia page METHOD 1
       let url2 = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=River_Dee,_Aberdeenshire'
-      axios.get(url2)
-        .then(
-          response => response.data
-        )
+      let pageExtract = await axios.get(url2)
+      console.log(pageExtract)
+      // look up wikipedia page
       // https://en.wikipedia.org/w/api.php?action=query&titles=belgrade&prop=extracts%7Cpageimages%7Cinfo&pithumbsize=400&inprop=url&redirects=&format=json&origin=*
-      let url3 = 'https://en.wikipedia.org/w/api.php?action=query&titles=River_Dee,_Aberdeenshire&prop=extracts%7Cpageimages%7Cinfo&pithumbsize=400&inprop=url&redirects=&format=json&origin=*'
-      this.wikipage = await axios.get(url3)
+      // wikipedia page info API call
+      let urlpageQuery = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + place + '&prop=extracts%7Cpageimages%7Cinfo&pithumbsize=400&inprop=url&redirects=&format=json&origin=*'
+      // console.log(urlpageQuery)
+      let rawWikipediaData = await axios.get(urlpageQuery)
+      // need to extract
+      let pageID = Object.keys(rawWikipediaData.data.query.pages)
+      // console.log(pageID)
+      this.wikipage = rawWikipediaData.data.query.pages[pageID[0]].extract
+      // console.log(this.wikipage)
+      // does a river, watershed, tributaries etc. mapping data exists?
+      this.checkBioregionDataMap(pageID[0])
+    },
+    checkBioregionDataMap (wikiID) {
+      console.log('check for bioregion map data')
+      console.log(wikiID)
+      let bioMapKeys = [{ wikid: '415350' }, { wikid: '166836' }, { wikid: '6668805' }, { wikid: 1237 }, { wikid: 1238 }]
+      this.matchMapData = bioMapKeys.some(u => u.wikid === wikiID)
+      console.log(this.matchMapData)
+    },
+    async lookupRiversScotland () {
+      console.log('lookup rivers')
+      // https://en.wikipedia.org/wiki/List_of_rivers_of_Scotland
+      let rivers = 'List_of_rivers_of_Scotland'
+      let urlriverQuery = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + rivers + '&prop=extracts%7Cpageimages%7Cinfo&pithumbsize=400&inprop=url&redirects=&format=json&origin=*'
+      // console.log(urlriverQuery)
+      this.rawRiverpage = await axios.get(urlriverQuery)
     }
   }
 }
@@ -89,5 +136,19 @@ export default {
 .bioregion-items li {
   height: 30px;
   width: 600px;
+}
+
+#message-postive {
+  border: 2px solid green;
+}
+
+#message-notready {
+  border: 2px solid orange;
+}
+
+#wikipage-page {
+  border: 1px solid red;
+  height: 360px;
+  overflow: scroll;
 }
 </style>
--- src/dashboard/vue.config.js
+++ src/dashboard/vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
     ? '/healthscience/code/diyhstoolkit/dist/'
     : '/', */
   devServer: {
-    // proxy: 'https://bioregion.healthscience.network/'  // online server setup
-    proxy: 'https://raw.githubusercontent.com/CodeTheCity/bioregional_dashboards/main/data/geojsonshed/'
+    proxy: 'https://bioregion.healthscience.network/'  // online server setup
+    // proxy: 'https://raw.githubusercontent.com/CodeTheCity/bioregional_dashboards/main/data/geojsonshed/'
     // proxy: 'https://en.wikipedia.org'
     // proxy: 'https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/'
   }
